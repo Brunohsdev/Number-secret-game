4a4b43d0b4fd1ec76cc1b6606cfbdbac13a77774
Brunohsdev <bhas795002@gmail.com>
2025-04-06 19:27:24 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-listaGenerica = ['paulo','bruno', ' gabriel'];
-listaDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
-
-
-listaDeProgramacao.push('Java','Ruby', 'GoLang');
-console.log(listaGenerica[1]);
-
